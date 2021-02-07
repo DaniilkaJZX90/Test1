b3d6eec4df3cc3674c5cfa03753dfc55ed04cbff
Daniil Yatsevsky <daniilka092@mail.ru>
2021-02-07 14:55:57 +0300

    Печатаем масси в консоль

--- JavaCore/src/geekbrains/lesson_4/Main.java
+++ JavaCore/src/geekbrains/lesson_4/Main.java
@@ -1,7 +1,13 @@
 package geekbrains.lesson_4;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
          int[] arr = new int[]{1, 2, 4, 5};
+
+        System.out.println(Arrays.toString(arr));
     }
+
+
 }
--- JavaCore/src/geekbrains/lesson_4/Test.java
+++
@@ -1,9 +0,0 @@
-package geekbrains.lesson_4;
-
-public class Test {
-    private  int test;
-
-    public Test(int test) {
-        this.test = test;
-    }
-}
